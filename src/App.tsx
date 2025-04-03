1ef71b80eda9303c540fafb42ce59dc9c9117f47
fujianhua <fujianhua09@gmail.com>
2025-04-03 17:48:51 +0800

    feat: 实现版本更新

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,38 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "@/pages/about";
 import "./styles/App.less";
 import Home from "@/pages/home";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    const version = localStorage.getItem("version");
+    if (!version) {
+      localStorage.setItem("version", "1.1.1");
+    }
+    setInit(true);
+  }, []);
+
+  useEffect(() => {
+    if (init) {
+      const version = localStorage.getItem("version");
+      const worker = new Worker("/version.js");
+      worker.postMessage({ type: "start", currentVersion: version });
+      worker.onmessage = (e) => {
+        const data = e.data;
+        if (data.type === "update") {
+          const result = confirm(`页面有更新，${data.versionContent}`);
+          if (result) {
+            localStorage.setItem("version", data.newVersion);
+            window.location.reload();
+          } else {
+            console.log("取消更新:");
+          }
+        }
+      };
+    }
+  }, [init]);
   return (
     <BrowserRouter>
       <Routes>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/variables.less';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/variables.less";
+import App from "./App";
+// import { createVersionPolling } from "version-polling";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// createVersionPolling({
+//   vcType: "versionJson",
+//   silent: import.meta.env.MODE === "development", // 开发环境下不检测
+//   onUpdate: (self) => {
+//     console.log('222:', 222);
+//     const result = confirm("页面有更新，点击确定刷新页面！");
+//     if (result) {
+//       self.onRefresh();
+//     } else {
+//       console.log("self:", self);
+//       console.log("result:", result);
+//       self.onCancel();
+//     }
+//   },
+// });
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
